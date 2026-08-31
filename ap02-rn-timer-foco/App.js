396e584cac76f8cfa8a92b778f809a26a1e9dfab
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-31 17:35:29 -0300

    learn(ap02-rn): encerra a apostila

--- ap02-rn-timer-foco/App.js
+++ ap02-rn-timer-foco/App.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react'
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  SafeAreaView
+} from 'react-native-safe-area-context'
 
 export default function App() {
   const [segundos, setSegundos] = useState(25 * 60)
   const [rodando, setRodando] = useState(false)
   useEffect(() => {
+    if(!rodando)return
     const id = setInterval(() => {
       setSegundos(s => s - 1)
     }, 1000)
@@ -13,17 +18,24 @@ export default function App() {
   const minutos = Math.floor(segundos / 60)
   const segs = segundos % 60
   return (
-    <View style={styles.container}>
-      <Text style={styles.tempo}>
-        {String(minutos).padStart(2, '0')}:{String(segs).padStart(2, '0')}
-      </Text>
-      <Pressable 
-        style={styles.botao}
-        onPress={() => setSegundos(segundos - 1)}>
-        <Text
-          style={styles.textoBotao}>-1 segundo</Text>
-      </Pressable>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.tempo}>
+          {String(minutos).padStart(2, '0')}:{String(segs).padStart(2, '0')}
+        </Text>
+        <Pressable 
+          style={styles.botao}
+          onPress={() => setRodando(r => !r)}>
+          <Text
+            style={styles.textoBotao}>{rodando ? 'Pausar' : 'Iniciar'}</Text>
+        </Pressable>
+        <Pressable 
+          style={styles.botaoSecundario}
+          onPress={() => {setRodando(false); setSegundos(25 * 60)}}>
+          <Text>Reiniciar</Text>
+        </Pressable>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -44,6 +56,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#087ea4',
     paddingVertical: 14,
     paddingHorizontal: 48,
+    borderRadius: 10,
+    marginBottom: 14
+  },
+  botaoSecundario: {
+    backgroundColor: '#334155',
+    paddingVertical: 14, paddingHorizontal: 48,
     borderRadius: 10
   },
   textoBotao: {color: '#FFFFFF', fontSize: 18, fontWeight: '600'}
